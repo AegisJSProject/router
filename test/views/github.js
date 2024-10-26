7a163819206b5e411907b690228ad2ebdfc3efda
Chris Zuber <shgysk8zer0@gmail.com>
2024-10-26 12:27:38 -0700

    Add new URL/routing functionality
    - Add `SearchParam` and `manageSearch` and `getSearch` for utilizing URL search params as state
    - Add `getNavController` and `getNavSignal` and `whenNavigated` for cleanup on navigation
    - Add more preloading/prefetch/preconnect/dns-prefetch related functions
    - Add functions for updating page title and description
    - Add Trust Policy for setting/parsing HTML
    - Add scroll restoration handling
    - Add cancellable navigation events (`NavigationEvent`) which, if `event.preventDefault` is called on, cancel navigation
    - Add support for clearing registered routes
    - Add support for directly registering functions/constructor - Useful for dynamic routes and redirects

--- test/views/github.js
+++ test/views/github.js
@@ -1,33 +1,38 @@
-import { html } from '@aegisjsproject/core/parsers/html.js';
 import { manageState } from '@aegisjsproject/state';
 
 const [users, setUsers] = manageState('github:users', {});
+const url = (strings, ...values) => String.raw(strings, ...values.map(val => encodeURIComponent(val)));
 
-export default async ({ matches }) => {
+export default async ({ matches, signal }) => {
 	const username = matches?.pathname?.groups?.username;
 
 	if (typeof username !== 'string') {
-		return html`<p>Error: No GitHub username provided.</p>`;
+		return `<p>Error: No GitHub username provided.</p>`;
 	} else if (! (username in users)) {
-		const apiUrl = `https://api.github.com/users/${encodeURIComponent(username)}`;
+		const apiUrl = url`https://api.github.com/users/${username}`;
 		const resp = await fetch(apiUrl, { referrerPolicy: 'no-referrer' });
 
 		if (!resp.ok) {
-			return html`<p>Error fetching GitHub user data: ${resp.statusText}</p>`;
+			return `<p>Error fetching GitHub user data: ${resp.statusText}</p>`;
 		}
+
 		setUsers({ ...users, [username]: await resp.json() });
 	}
 
-	return html`
+	signal.addEventListener('abort', ({ target }) => console.info(target.reason), { once: true });
+
+	return `
 		<section>
-			<h1>${users[username].name ?? username}</h1>
-			<p><strong>Username:</strong> ${users[username].login}</p>
-			<p><strong>Bio:</strong> ${users[username].bio ?? 'No bio available'}</p>
-			<p><strong>Public Repos:</strong> ${users[username].public_repos}</p>
-			<p><strong>Followers:</strong> ${users[username].followers}</p>
-			<p><strong>Following:</strong> ${users[username].following}</p>
-			${users[username].blog ? `<p><a href="${users[username].blog}" target="_blank">Website</a></p>` : ''}
-			${users[username].avatar_url ? `<img src="${users[username].avatar_url}" alt="Avatar" crossorigin="anonymous" referrerpolicy="no-referrer" loading="lazy" width="100" />` : ''}
+			<template shadowrootmode="open">
+				<h1>${users[username].name ?? username}</h1>
+				<p><strong>Username:</strong> ${users[username].login}</p>
+				<p><strong>Bio:</strong> ${users[username].bio ?? 'No bio available'}</p>
+				<p><strong>Public Repos:</strong> ${users[username].public_repos}</p>
+				<p><strong>Followers:</strong> ${users[username].followers}</p>
+				<p><strong>Following:</strong> ${users[username].following}</p>
+				${users[username].blog ? `<p><a href="${users[username].blog}" target="_blank">Website</a></p>` : ''}
+				${users[username].avatar_url ? `<img src="${users[username].avatar_url}" alt="Avatar" crossorigin="anonymous" referrerpolicy="no-referrer" loading="lazy" width="100" />` : ''}
+			</template>
 		</section>
 	`;
 };
